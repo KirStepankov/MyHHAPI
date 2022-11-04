be186f79358b2d235544840c5f882eb8e47cdc3a
Кирилл <mr.mr.kir@gmail.com>
2022-11-04 19:16:23 +0200

    Фикс запросов

--- src/MyHHAPIAbstract.php
+++ src/MyHHAPIAbstract.php
@@ -123,7 +123,7 @@ abstract class MyHHAPIAbstract
         $curl->get($url);
 
         return $curl->error
-            ? throw new Exception("Ошибка при запросе в АПИ HH: $url", $curl->error_code)
+            ? throw new Exception("Ошибка при запросе в АПИ HH: $url ($curl->error_code)")
             : json_decode($curl->response, true);
     }
 
